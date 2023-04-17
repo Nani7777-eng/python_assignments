324df2b28d19c7e8beab2b084f42117a8753d95a
Lok Venkatesh <81313292+Nani7777-eng@users.noreply.github.com>
2023-04-17 09:46:11 +0530

    Update sum of element wise diff between 2 lists .py

--- sum of element wise diff between 2 lists .py
+++ sum of element wise diff between 2 lists .py
@@ -1,7 +1,8 @@
+
 lis = [5,12,5,6,2,6,7,8]
 lis2 = [6,4,3,8,1,2,4,6]
 sub_list = []
-sum  = 0
+
 
 for (i,j) in zip(lis,lis2):
     if i>j:
@@ -11,7 +12,6 @@ for (i,j) in zip(lis,lis2):
         k = j-i
         sub_list.append(k)
 print(sub_list)
-for p in range(len(sub_list)):
-    sum+=sub_list[p]
 
-print(sum)
+total = sum(sub_list)
+print(total)
